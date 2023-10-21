3ca2f2c21aefd131503731d78a6ce1f1a832adbb
Viateur <akimanaviateur94@gmail.com>
2023-10-21 10:30:44 +0200

    constructors

--- src/object/oriented/programming/App.java
+++ src/object/oriented/programming/App.java
@@ -6,13 +6,6 @@ public class App {
         System.out.println(peter);
         // Create a Person of type Employee with a Default Constructor
         //Not Possible to set additional information of employee
-        Person p1=new Employee();
-        p1.setFirstName("MUGISHA");
-        p1.setLastName("Davis");
-        p1.setAge(18);
-        System.out.println(p1);
-
-        //Create Employee, Now you can set all properties of employee
         Employee p2=new Employee();
         p2.setAge(18);
         p2.setFirstName("Mahoro");
@@ -23,7 +16,18 @@ public class App {
         System.out.println(p2);
         System.out.println(p2.getFirstName());
         System.out.println(p2.getInstitution());
+        Person p3=new Employee("RCA","Instructor",120, 18,"MUGISHA","Davis");
+        System.out.println(p3);
+        //Create a Student
+        Person p4=new Student("Mugisha","Manzi","S5","RCA","SPE");
+        System.out.println(p4.getFirstName());
+        System.out.println(p4);
+        //Everything is an Object
+        Object p5=new Employee("Nyabihu","Nurse",120,18,"Mugabo","Mike");
+        System.out.println(p5);
     }
 
 }
+
+
 
